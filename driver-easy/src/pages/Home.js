495a653f7e9165be3bb445678484eff6e0d0cafc
Joao Carlos <61249758+devjaum@users.noreply.github.com>
2024-05-13 14:10:09 +0000

    main

---
+++ driver-easy/src/pages/Home.js
@@ -0,0 +1,18 @@
+import "../styles/homeStyle.css"
+
+function Home(){
+    return(
+        <main class="main">
+            <div class="oppacity">
+                <p>HISTÓRIA</p>
+                <p>REGRAS</p>
+                <p>PERSONAGENS</p>
+                <p>ITENS</p>
+                <p>COMBATE</p>
+                <p>CONTATO</p>
+            </div>
+        </main>
+    )
+}
+
+export default Home
